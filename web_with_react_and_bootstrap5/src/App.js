60af11ecf92451a6dc9d885651953cca4845a98b
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-11-22 07:05:11 -0600

    feat: Initialize React and Bootstrap5

---
+++ web_with_react_and_bootstrap5/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to React and Bootstrap 5</h1>
+      <div className='card shadow'>
+        <div className='card-body'>
+          <a className='btn btn-primary' href='https://reactjs.org/'>
+            Click here to learn more about React!
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
