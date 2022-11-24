f08a223957c6a5072afae9e41d672e659808de85
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-11-24 03:47:12 -0600

    feat: Initialized React & Bootstrap 5

---
+++ bootstrap5_course_with_traversy-media/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to Bootstrap 5 with Traversy Media !</h1>
+    </div>
+  );
+}
+
+export default App;
